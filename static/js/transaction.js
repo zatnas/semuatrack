7e405decfe8a125e6f78709ef92e2f4111c50f16
zethnest <16971676+zethnest@users.noreply.github.com>
2022-09-16 18:26:45 +0800

    feat: datetime mode

--- static/js/transaction.js
+++ static/js/transaction.js
@@ -38,6 +38,7 @@ fetch("/api/transaction")
 
 
 // edit transaction
+// DOM transaction_list
 transaction_list.addEventListener("dblclick", function(click) {
 	console.log(click);
 });
@@ -47,7 +48,6 @@ transaction_list.addEventListener("dblclick", function(click) {
 let datetime_input = document.querySelector("#datetime_input");
 let date_input = document.querySelector("#date_input");
 let time_input = document.querySelector("#time_input");
-
 function datetime_value(
 	date_string = (new Date()).toDateString(),
 	time_string = ""
@@ -71,3 +71,65 @@ date_input.addEventListener("change",
 	() => set_datetime_value(datetime_input, date_input, time_input));
 time_input.addEventListener("change",
 	() => set_datetime_value(datetime_input, date_input, time_input));
+
+
+// datetime mode
+let datetime_mode = document.querySelector("#datetime_mode");
+let datetime_modes = document.querySelector("#datetime_modes");
+let datetime_now_button = document.querySelector("#datetime_now_button");
+let datetime_custom_button = document.querySelector("#datetime_custom_button");
+
+let current_datetime_mode = datetime_mode.textContent;
+
+let update_datetime_timeout;
+function update_datetime_input(once) {
+	if (current_datetime_mode != "Now") {
+		return;
+	}
+	let now = new Date();
+	let now_date = "";
+	now_date += `${String(now.getFullYear()).padStart(2, '0')}-`;
+	now_date += `${String(now.getMonth()).padStart(2, '0')}-`;
+	now_date += `${String(now.getDate()).padStart(2, '0')}`;
+	let now_time = "";
+	now_time += `${String(now.getHours()).padStart(2, '0')}:`;
+	now_time += `${String(now.getMinutes()).padStart(2, '0')}`;
+	date_input.value = now_date;
+	time_input.value = now_time;
+	set_datetime_value(datetime_input, date_input, time_input);
+	if (once) {
+		return;
+	}
+	start_update_datetime_input();
+}
+
+function start_update_datetime_input() {
+	update_datetime_input(true);
+	update_datetime_timeout = setTimeout(
+		update_datetime_input,
+		1000, // Milliseconds
+	)
+}
+
+function stop_update_datetime_input() {
+	clearTimeout(update_datetime_timeout);
+}
+
+if (current_datetime_mode == "Now") {
+	start_update_datetime_input();
+}
+
+datetime_now_button.addEventListener("click", () => {
+	datetime_mode.textContent = "Now";
+	datetime_custom_button.classList.remove("active");
+	datetime_now_button.classList.add("active");
+	start_update_datetime_input();
+});
+datetime_custom_button.addEventListener("click", () => {
+	datetime_mode.textContent = "Custom";
+	datetime_now_button.classList.remove("active");
+	datetime_custom_button.classList.add("active");
+	date_input.value = "";
+	time_input.value = "";
+	stop_update_datetime_input();
+});
