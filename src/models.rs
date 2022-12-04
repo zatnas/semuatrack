893e0a438de655eb73446ead72315a3c36ec03f7
Izzat Nasrun <16971676+zatnas@users.noreply.github.com>
2022-12-04 16:26:12 +0800

    feat: category struct

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use super::schema::*;
+use diesel::prelude::Queryable;
 use diesel::prelude::*;
 use serde::Serialize;
 
@@ -19,4 +20,20 @@ pub struct NewCashflow<'r> {
     pub amount: f32,
     pub note: Option<&'r str>,
     pub place: Option<&'r str>,
+}
+
+#[derive(Debug, Queryable, Serialize)]
+pub struct Category {
+    pub id: i32,
+    pub name: String,
+    pub color: Option<String>,
+    pub icon: Option<String>,
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = category)]
+pub struct NewCategory {
+    pub name: String,
+    pub color: String,
+    pub icon: String,
 }
